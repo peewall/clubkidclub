07ba8fc19452f93349766d016830253b7ed949c8
Pete Wallace <peter.wallace@underbelly.is>
2023-01-19 14:54:31 -0500

    padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 
 
 </script>
-<div class="flex flex-col items-center justify-center mt-12 lg:mt-24 select-none">
+<div class="flex flex-col items-center justify-center mt-12 xl:mt-24 select-none">
     <img class="max-w-[300px]" alt="The project wordmark" src={wordmark} />
-    <img class="lg:w-[400px] w-[250px]" alt="The project logo" src={logo} />
+    <img class="lg:w-[300px] w-[250px]" alt="The project logo" src={logo} />
     <a href="/the-club">
         <img class="lg:w-[200px] w-[100px]" alt="enter button" src={enter} />
     </a>
